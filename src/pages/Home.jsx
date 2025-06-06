548797c94fc39d974d7535dd3cb275a5fab8c1b8
Rabi Sheestha <psychrabi@gmail.com>
2025-06-06 10:27:00 +0545

    Update project structure and content: change Node.js version to 20, update Navbar links to include '/rabistha/', enhance Features page with a new layout and feature descriptions, modify Home page content, and adjust router paths accordingly.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import Download from '@/components/Download'
 import Testimonials from '@/components/Testimonials';
+import {useCallback} from 'react';
+import { useNavigate } from 'react-router-dom';
 function Home() {
+  const navigate = useNavigate()
+
+  const handleDownload = useCallback(() => {
+    navigate('/rabistha/download')
+  }, []);
+
   return (
     <div>
        <div className="container px-6 py-16 mx-auto">
         <div className="items-center lg:flex">
             <div className="w-full lg:w-1/2">
                 <div className="lg:max-w-lg">
-                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Best place to choose <br /> your <span className="text-blue-500 ">clothes</span></h1>
+                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl"><span className="text-blue-500 ">ASTER Multiseat</span></h1>
                     
-                    <p className="mt-3 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                    <p className="mt-3 text-gray-600 dark:text-gray-400">Turn one computer into two, four, six or even 12 workplaces, working independantly with each other.</p>
                     
-                    <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
+                    <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500" onClick={() => handleDownload()}>Try Now</button>
                 </div>
             </div>
 
