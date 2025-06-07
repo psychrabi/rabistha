1e093c563b3f7172ac8de8c2a7be5dabf8c076d3
Rabi Sheestha <psychrabi@gmail.com>
2025-06-07 18:08:48 +0545

    updated navbar to use array for links, moved navlink to its own file

---
+++ src/data/navLinks.js
@@ -0,0 +1,10 @@
+export const navLinks = [
+  { label: "Home", to: "/rabistha/", enabled: true },
+  { label: "Features", to: "/rabistha/features", enabled: true },
+  { label: "Purchase", to: "/rabistha/purchase", enabled: true },
+  { label: "FAQs", to: "/rabistha/faqs", enabled: true },
+  { label: "Contact", to: "/rabistha/contact", enabled: true },
+  { label: "Wiki", to: "/rabistha/wiki", enabled: false },
+  { label: "Blog", to: "/rabistha/blog", enabled: false },
+  { label: "News", to: "/rabistha/news", enabled: false },
+];
