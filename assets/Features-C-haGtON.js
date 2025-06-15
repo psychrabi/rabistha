0357f31ffc921dd4ede74e062d85caaeabab8252
psychrabi <psychrabi@users.noreply.github.com>
2025-06-15 18:33:02 +0000

    deploy: 4421f279ba2ee936558214889e8b4f5b7cd90be8

---
+++ assets/Features-C-haGtON.js
@@ -0,0 +1,36 @@
+import{c as t,j as e}from"./index-B2jBjsIj.js";import{N as o}from"./network-FW5QNoEH.js";import{M as n}from"./monitor-Dqpkrh9Z.js";import{D as s}from"./Download-BkTub3CA.js";/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M12 18H4a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5",key:"x6cv4u"}],["path",{d:"m16 19 3 3 3-3",key:"1ibux0"}],["path",{d:"M18 12h.01",key:"yjnet6"}],["path",{d:"M19 16v6",key:"tddt3s"}],["path",{d:"M6 12h.01",key:"c2rlol"}],["circle",{cx:"12",cy:"12",r:"2",key:"1c9p78"}]],l=t("banknote-arrow-down",c);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M12 2a10 10 0 0 1 7.38 16.75",key:"175t95"}],["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["path",{d:"M2.5 8.875a10 10 0 0 0-.5 3",key:"1vce0s"}],["path",{d:"M2.83 16a10 10 0 0 0 2.43 3.4",key:"o3fkw4"}],["path",{d:"M4.636 5.235a10 10 0 0 1 .891-.857",key:"1szpfk"}],["path",{d:"M8.644 21.42a10 10 0 0 0 7.631-.38",key:"9yhvd4"}]],r=t("clock-fading",d);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=[["path",{d:"M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z",key:"nnexq3"}],["path",{d:"M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12",key:"mt58a7"}]],h=t("leaf",p);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=[["path",{d:"M6.3 20.3a2.4 2.4 0 0 0 3.4 0L12 18l-6-6-2.3 2.3a2.4 2.4 0 0 0 0 3.4Z",key:"goz73y"}],["path",{d:"m2 22 3-3",key:"19mgm9"}],["path",{d:"M7.5 13.5 10 11",key:"7xgeeb"}],["path",{d:"M10.5 16.5 13 14",key:"10btkg"}],["path",{d:"m18 3-4 4h6l-4 4",key:"16psg9"}]],y=t("plug-zap",k);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=[["path",{d:"M12 15v-3.014",key:"aw6ppf"}],["path",{d:"M16 15v-3.014",key:"9e0vc7"}],["path",{d:"M20 6H4",key:"1lfz86"}],["path",{d:"M20 8V4",key:"1l2g47"}],["path",{d:"M4 8V4",key:"sppxzt"}],["path",{d:"M8 15v-3.014",key:"when08"}],["rect",{x:"3",y:"12",width:"18",height:"7",rx:"1",key:"1ucwdz"}]],u=t("ruler-dimension-line",m);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["path",{d:"M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z",key:"4pj2yx"}],["path",{d:"M20 3v4",key:"1olli1"}],["path",{d:"M22 5h-4",key:"1gvqau"}],["path",{d:"M4 17v2",key:"vumght"}],["path",{d:"M5 18H3",key:"zchphs"}]],g=t("sparkles",x);/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=[["path",{d:"M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z",key:"uqj9uw"}],["path",{d:"M16 9a5 5 0 0 1 0 6",key:"1q6k2b"}]],v=t("volume-1",M),w=[{icon:l,title:"Cost Reduction",description:"Savings up to 70% on creating a computer workstation"},{icon:y,title:"Electricity Saving",description:"Using ASTER you can reduce your electricity bill up to 4 times"},{icon:h,title:"Eco Friendly",description:"We will help you save nature"},{icon:u,title:"Space Saving",description:"You can create an additional workplace even in a small room"},{icon:r,title:"Quick Deployment",description:"Installing ASTER does not require formatting the disk or reinstalling the Windows"},{icon:g,title:"Easy to use",description:"ASTER can be started or unloaded at any time as a usual program"},{icon:o,title:"Zero Network Load",description:"Data exchange between workplaces does not increase network traffic"},{icon:n,title:"Alternative to thin clients",description:"6 workstations on a computer without purchasing thin clients"},{icon:v,title:"Low Noise Level",description:"Multiseat systems reduce noise levels by eliminating additional system units"}];function _(){return e.jsxs("section",{className:"bg-white dark:bg-gray-900",children:[e.jsxs("div",{className:"container px-6 py-10 mx-auto",children:[e.jsxs("h1",{className:"text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white text-center",children:["What makes ",e.jsx("span",{className:"text-blue-500",children:"ASTER"})," great"]}),e.jsx("div",{className:"grid grid-cols-1 gap-6 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-3",children:w.map((a,i)=>e.jsxs("div",{className:"flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800",children:[e.jsx("span",{className:"inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500",children:e.jsx(a.icon,{})}),e.jsx("h1",{className:"text-xl font-semibold text-gray-700 capitalize dark:text-white",children:a.title}),e.jsx("p",{className:"text-gray-500 dark:text-gray-300",children:a.description})]},i))})]}),e.jsx(s,{})]})}export{_ as default};
