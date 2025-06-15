e341b9abd283476b6da651e2aa0aeef5e3044915
Rabi Sheestha <psychrabi@gmail.com>
2025-06-15 11:30:06 +0545

    asd

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/rabistha/",
+  base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
