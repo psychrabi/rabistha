906504e74413870c7d8d8154b2b8e9d68af254af
Rabi Sheestha <psychrabi@gmail.com>
2025-06-15 11:24:22 +0545

    revert changes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/rabistha/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
