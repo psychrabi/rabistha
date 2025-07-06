f6553c317ed546af0a788a9bad076aca5ca2855f
psychrabi <psychrabi@users.noreply.github.com>
2025-07-06 09:19:56 +0000

    deploy: df1f9ed42f9273b930bad3a992c9e0626027a4ee

---
+++ assets/MatrixCard-8_nItxJa.js
@@ -0,0 +1,6 @@
+import{c as d,R as l,j as e}from"./index-TPnzWnce.js";/**
+ * @license lucide-react v0.513.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["path",{d:"m15.5 7.5 2.3 2.3a1 1 0 0 0 1.4 0l2.1-2.1a1 1 0 0 0 0-1.4L19 4",key:"g0fldk"}],["path",{d:"m21 2-9.6 9.6",key:"1j0ho8"}],["circle",{cx:"7.5",cy:"15.5",r:"5.5",key:"yqb3hr"}]],n=d("key",x),o=l.memo(({data:a})=>{const{title:s,value:t,color:r,Icon:c}=a;return e.jsx("div",{className:"rounded-lg  bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 ",children:e.jsx("div",{className:"p-6",children:e.jsxs("div",{className:"flex items-center justify-between",children:[e.jsxs("div",{children:[e.jsx("p",{className:"text-sm font-medium text-gray-600 dark:text-gray-300",children:s}),e.jsx("p",{className:"text-3xl font-bold text-gray-900 dark:text-gray-100",children:t})]}),e.jsx("div",{className:"bg-blue-100 p-3 rounded-lg",children:e.jsx(c,{className:`${r} h-6 w-6`})})]})})})});export{n as K,o as M};
---
+++ assets/News-DU3zhflU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-TPnzWnce.js";function s(){return e.jsx("div",{children:"News"})}export{s as default};
